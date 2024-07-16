b0443160819a5da52867d70b55d3ae4b3070f7eb
Diegx Perez <129129662+diegxperez@users.noreply.github.com>
2024-07-16 16:45:29 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brandprimary: "#ECF15E",
+        brandsecondary: "#D6DD13",
+        blacksoft: "#0E0F11",
+        accentaccent: "#8996A9",
+        accentsecondary: "#ECF15E",
+      },
+      fontFamily: {
+        sans: ["Onest", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
