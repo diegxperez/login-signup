ff015e2710e51803c0c4b8da2714205634a636e7
Diegx Perez <129129662+diegxperez@users.noreply.github.com>
2024-07-24 23:37:11 -0500

    feat: add error message for empty field on blur event

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const inputPassword = document.getElementById("input-password");
+// TooglePassword
 const showPassword = document.getElementById("show-password");
 const hidePassword = document.getElementById("hide-password");
 const tooglePassword = document.getElementById("tooglePassword");
+
+// Form
+const signUpForm = document.getElementById("sign-form");
+const inputFirstName = document.getElementById("first-name-signup");
+const inputLastName = document.getElementById("last-name-signup");
+const inputEmail = document.getElementById("email-signup");
+const inputPassword = document.getElementById("password-signup");
+const btnSubmitSignUp = document.getElementById("btn-submit-signup");
+//
+const containerPassword = document.getElementById("container-password");
 
 tooglePassword.addEventListener("click", () => {
   if (inputPassword.getAttribute("type") === "password") {
@@ -21,3 +31,33 @@ function toogleVisibility(element) {
     element.classList.add("hidden");
   }
 }
+
+// btnSubmit.addEventListener("click", () => {
+//   signUpForm.submit();
+// });
+
+inputFirstName.addEventListener("blur", function (e) {
+  const field = e.target;
+  const fieldValue = e.target.value;
+
+  const existingErrorName = document.querySelector(".error-name");
+
+  if (fieldValue.length === 0) {
+    if (!existingErrorName) {
+      const errorName = document.createElement("p");
+      errorName.innerText = "Ingrese un nombre valido";
+      errorName.classList.add(
+        "text-xs",
+        "font-light",
+        "opacity-60",
+        "text-brandprimary",
+        "error-name",
+      );
+      containerPassword.appendChild(errorName);
+    }
+  } else {
+    if (existingErrorName) {
+      containerPassword.removeChild(existingErrorName);
+    }
+  }
+});
