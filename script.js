a275f0b4bcbe2894fa70ea03cb043c69c08da4cc
Diegx Perez <129129662+diegxperez@users.noreply.github.com>
2024-07-29 23:15:20 -0500

    Add validate form

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ const inputLastName = document.getElementById("last-name-signup");
 const inputEmail = document.getElementById("email-signup");
 const inputPassword = document.getElementById("password-signup");
 const btnSubmitSignUp = document.getElementById("btn-submit-signup");
-//
+// Container Password
 const containerPassword = document.getElementById("container-password");
 
-tooglePassword.addEventListener("click", () => {
+const validateTooglePassword = () => {
   if (inputPassword.getAttribute("type") === "password") {
     inputPassword.setAttribute("type", "text");
   } else {
@@ -22,42 +22,109 @@ tooglePassword.addEventListener("click", () => {
 
   toogleVisibility(showPassword);
   toogleVisibility(hidePassword);
-});
 
-function toogleVisibility(element) {
-  if (element.classList.contains("hidden")) {
-    element.classList.replace("hidden", "block");
-  } else {
-    element.classList.add("hidden");
+  function toogleVisibility(element) {
+    if (element.classList.contains("hidden")) {
+      element.classList.replace("hidden", "block");
+    } else {
+      element.classList.add("hidden");
+    }
   }
-}
+};
 
-// btnSubmit.addEventListener("click", () => {
-//   signUpForm.submit();
-// });
+tooglePassword.addEventListener("click", validateTooglePassword);
 
-inputFirstName.addEventListener("blur", function (e) {
+// const validateEmptyField = (e) => {
+//   const field = e.target;
+//   const fieldValue = e.target.value;
+
+//   const parentField = field.parentElement;
+
+//   if (fieldValue.trim().length === 0) {
+//     parentField.classList.add("outline", "outline-1", "outline-red-600");
+//     console.log(parentField);
+//   } else {
+//     parentField.classList.remove("outline", "outline-1", "outline-red-600");
+//   }
+// };
+
+const regexEmail = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+const regexPassword = new RegExp(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/,
+);
+const regexName = new RegExp(
+  /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/,
+);
+
+const validateFormat = (regex, e) => {
   const field = e.target;
   const fieldValue = e.target.value;
+  const parentField = field.parentElement;
 
-  const existingErrorName = document.querySelector(".error-name");
-
-  if (fieldValue.length === 0) {
-    if (!existingErrorName) {
-      const errorName = document.createElement("p");
-      errorName.innerText = "Ingrese un nombre valido";
-      errorName.classList.add(
-        "text-xs",
-        "font-light",
-        "opacity-60",
-        "text-brandprimary",
-        "error-name",
-      );
-      containerPassword.appendChild(errorName);
-    }
+  if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
+    parentField.classList.add("outline", "outline-1", "outline-red-600");
   } else {
-    if (existingErrorName) {
-      containerPassword.removeChild(existingErrorName);
-    }
+    parentField.classList.remove("outline", "outline-1", "outline-red-600");
+    parentField.classList.add("outline", "outline-1", "outline-brandprimary");
   }
+};
+
+inputFirstName.addEventListener("input", (e) => validateFormat(regexName, e));
+inputLastName.addEventListener("input", (e) => validateFormat(regexName, e));
+inputEmail.addEventListener("input", (e) => validateFormat(regexEmail, e));
+inputPassword.addEventListener("input", (e) =>
+  validateFormat(regexPassword, e),
+);
+
+btnSubmitSignUp.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (validateForm()) {
+    console.log("registrado con exito");
+  }
+  // const firstname = inputFirstName.value.trim();
+  // const lastname = inputLastName.value.trim();
+  // const email = inputEmail.value.trim();
+  // const password = inputPassword.value;
+  // console.log(firstname);
 });
+
+const validateForm = () => {
+  let isValid = true;
+
+  const fields = [inputFirstName, inputLastName, inputEmail, inputPassword];
+
+  fields.forEach((field) => {
+    const fieldValue = field.value.trim();
+    const parentField = field.parentElement;
+
+    if (fieldValue.length === 0) {
+      parentField.classList.add("outline", "outline-1", "outline-red-600");
+      isValid = false;
+    } else {
+      parentField.classList.remove("outline", "outline-1", "outline-red-600");
+    }
+
+    if (field === inputFirstName && !regexName.test(fieldValue)) {
+      parentField.classList.add("outline", "outline-1", "outline-red-600");
+      isValid = false;
+    }
+
+    if (field === inputLastName && !regexName.test(fieldValue)) {
+      parentField.classList.add("outline", "outline-1", "outline-red-600");
+      isValid = false;
+    }
+
+    if (field === inputEmail && !regexEmail.test(fieldValue)) {
+      parentField.classList.add("outline", "outline-1", "outline-red-600");
+      isValid = false;
+    }
+
+    if (field === inputPassword && !regexPassword.test(fieldValue)) {
+      parentField.classList.add("outline", "outline-1", "outline-red-600");
+      isValid = false;
+    }
+  });
+
+  return isValid;
+};
